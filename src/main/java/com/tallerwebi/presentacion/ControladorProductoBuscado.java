d85ae2b2bcc52aeb10198501e4c5c1aee7cc227a
Guarnieri Matias <matiasnguarnieri@hotmail.com>
2024-05-07 08:53:46 -0300

    avances en producto buscado - controlador producto buscado

---
+++ src/main/java/com/tallerwebi/presentacion/ControladorProductoBuscado.java
@@ -0,0 +1,16 @@
+package com.tallerwebi.presentacion;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ControladorProductoBuscado {
+    @RequestMapping(path = "/productobuscado", method = RequestMethod.GET)
+    public ModelAndView irAHome() {
+        ModelAndView modelAndView = new ModelAndView("productobuscado");
+        return modelAndView;
+    }
+}
