7335b3a716d5669f69e9b41b16bf76f407e68770
Gonza-Jordan <57627092+Gonza-Jordan@users.noreply.github.com>
2024-06-17 16:25:45 -0300

    Agrego JS de carrito

--- src/main/webapp/resources/core/js/main.js
+++ src/main/webapp/resources/core/js/main.js
@@ -7,48 +7,4 @@ function crearMapa(contenedor, latitud, longitud) {
     }).addTo(map);
 }
 
-/* ******************************************* FUNCIONES CHANGUITO ********************************************* */
-
-function incrementarCantidad(index, precio) {
-    var inputCantidad = document.getElementById('cantidad-' + index);
-    var cantidad = parseInt(inputCantidad.value);
-    cantidad++;
-    inputCantidad.value = cantidad;
-    actualizarPrecio(index, cantidad, precio);
-}
-
-function decrementarCantidad(index, precio) {
-    var inputCantidad = document.getElementById('cantidad-' + index);
-    var cantidad = parseInt(inputCantidad.value);
-    if (cantidad > 1) {
-        cantidad--;
-        inputCantidad.value = cantidad;
-        actualizarPrecio(index, cantidad, precio);
-    }
-}
-
-function actualizarPrecio(index, cantidad, precioUnitario) {
-    var inputCantidad = document.getElementById('cantidad-' + index);
-    var cantidad = parseInt(inputCantidad.value);
-
-    var precioTotal = cantidad * precioUnitario;
-    var precioElemento = document.getElementById('precio-unitario-' + index);
-    precioElemento.innerText = precioTotal.toFixed(2);
-
-    // Recalcular el total
-    recalcularTotal();
-}
-
-function recalcularTotal() {
-    var preciosUnitarios = document.querySelectorAll('[id^="precio-unitario-"]');
-    var total = 0;
-    preciosUnitarios.forEach(function(element) {
-        total += parseFloat(element.innerText);
-    });
-
-    // Actualizar el total final
-    var totalFinalElement = document.getElementById('total-final');
-    totalFinalElement.innerText = total.toFixed(2);
-}
-
 // showSlides();
