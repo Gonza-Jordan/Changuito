a7edd96c581c08dd18aae42e126582ecaa254e24
Gonza-Jordan <57627092+Gonza-Jordan@users.noreply.github.com>
2024-07-02 22:44:56 -0300

    Envio de mails exitoso

--- src/main/java/com/tallerwebi/config/MailConfig.java
+++ src/main/java/com/tallerwebi/config/MailConfig.java
@@ -1,4 +1,4 @@
-package com.tallerwebi.dominio;
+package com.tallerwebi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/tallerwebi/presentacion/ControladorEmail.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorEmail.java
@@ -2,18 +2,23 @@ package com.tallerwebi.presentacion;
 
 import com.tallerwebi.dominio.ServicioEmail;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ControladorEmail {
 
     @Autowired
     private ServicioEmail servicioEmail;
 
     @GetMapping("/send-email")
-    public String sendEmail() {
-        servicioEmail.sendSimpleMessage("destinatario@example.com", "Asunto", "Contenido del mensaje");
-        return "emailEnviado";
+    public String sendEmail(@RequestParam String to, @RequestParam String subject, @RequestParam String body) {
+        try {
+            servicioEmail.sendSimpleMessage(to, subject, body);
+            return "Email enviado exitosamente!";
+        } catch (Exception e) {
+            return "Error enviando email: " + e.getMessage();
+        }
     }
 }
