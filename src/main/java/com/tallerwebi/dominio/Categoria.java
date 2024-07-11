7679c2c9dd15d3cb333a09780514fcbec6eebe30
Lucas-Lilla <llilla@alumno.unlam.edu.ar>
2024-07-11 14:31:31 -0300

    BD con más SupermercadoProducto - Arreglo de estilos

--- src/main/java/com/tallerwebi/dominio/Categoria.java
+++ src/main/java/com/tallerwebi/dominio/Categoria.java
@@ -3,10 +3,10 @@ package com.tallerwebi.dominio;
 public enum Categoria {
 
     Almacen(Subcategoria.Arroz, Subcategoria.Aceite, Subcategoria.Harina, Subcategoria.Pastas, Subcategoria.Endulzantes),
-    Perfumeria(Subcategoria.Toallitas, Subcategoria.Acondicionador, Subcategoria.Shampoo, Subcategoria.Dentifricos),
+    Perfumeria(Subcategoria.Acondicionador, Subcategoria.Shampoo, Subcategoria.Dentifricos),
     Bebidas(Subcategoria.Agua, Subcategoria.Gaseosas, Subcategoria.Jugos, Subcategoria.Vinos, Subcategoria.Cervezas),
     Verduleria(Subcategoria.Tomate, Subcategoria.Manzana, Subcategoria.Lechuga, Subcategoria.Zanahoria, Subcategoria.Papa),
-    Limpieza(Subcategoria.Guantes, Subcategoria.Servilletas, Subcategoria.Escobas, Subcategoria.Lavandina, Subcategoria.Detergente, Subcategoria.Esponjas, Subcategoria.Trapos),
+    Limpieza(Subcategoria.Guantes, Subcategoria.Lavandina, Subcategoria.Detergente, Subcategoria.Esponjas, Subcategoria.Trapos),
     Lacteos(Subcategoria.Leche, Subcategoria.Manteca, Subcategoria.Quesos, Subcategoria.Yogures, Subcategoria.Crema);
 
     private final Subcategoria[] subcategorias;
--- src/main/java/com/tallerwebi/dominio/Subcategoria.java
+++ src/main/java/com/tallerwebi/dominio/Subcategoria.java
@@ -4,9 +4,9 @@ public enum Subcategoria {
 
     Harina, Pastas, Arroz, Aceite, Endulzantes,
     Gaseosas, Agua, Jugos, Vinos, Cervezas,
-    Dentifricos, Toallitas, Shampoo, Acondicionador, Jabones, Desodorantes,
+    Dentifricos, Shampoo, Acondicionador, Jabones, Desodorantes,
     Tomate, Manzana, Lechuga, Zanahoria, Papa,
-    Guantes, Servilletas, Escobas, Lavandina, Detergente, Esponjas, Trapos,
+    Guantes, Lavandina, Detergente, Esponjas, Trapos,
     Leche, Manteca, Quesos, Yogures, Crema
 
 }
